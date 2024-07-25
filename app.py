822d35538429520d6cb69973e5df78391c3ed642
Kavyanshg1 <166997759+Kavyanshg1@users.noreply.github.com>
2024-07-25 06:17:14 +0000

    commit

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ def logout():
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8080)
---
+++ replit.nix
@@ -0,0 +1,7 @@
+{pkgs}: {
+  deps = [
+    pkgs.nixVersions.nix_2_14
+    pkgs.nixVersions.nix_2_10
+    pkgs.nixVersions.minimum
+  ];
+}
